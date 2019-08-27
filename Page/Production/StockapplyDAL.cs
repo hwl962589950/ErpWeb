3b159969a63c0bdf4df81be9d278cb67a38bc4e6
黄威龙 <962589950@qq.com>
2019-08-27 11:03:45 +0800

    Initial commit

---
+++ Page/Production/StockapplyDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL.Production;
+using MysqlModel;
+
+namespace DAL.Production
+{
+   public partial  class StockapplyDAL:BaseDAL<stockapply>, IStockapplyDAL
+    {
+        
+
+    }
+}
